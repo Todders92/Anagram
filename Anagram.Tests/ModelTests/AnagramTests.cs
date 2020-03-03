4bc862aba6a3cd5f177771668fc9342b23095639
Brandan Sayarath and Todd Walraven <brandan@>
2020-03-03 15:16:08 -0800

    pass second test

--- Anagram.Tests/ModelTests/AnagramTests.cs
+++ Anagram.Tests/ModelTests/AnagramTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anagram.Models;
+using System.Collections.Generic;
+using System;
 
 namespace Anagram.Tests
 {
@@ -8,11 +10,27 @@ namespace Anagram.Tests
   {
 
     [TestMethod]
-    public void FormInput_TakesInUserinput_String()
+    public void AnagramFinder_TakesInUserinput_String()
     {
-      AnagramFinder anagramFinder = new AnagramFinder("dog");
+      AnagramFinder anagramFinder = new AnagramFinder("golden girl");
       Assert.AreEqual(typeof(AnagramFinder), anagramFinder.GetType());
     }
 
+    [TestMethod]
+      public void AddWordsList_ReturnsWords_String()
+      {
+        //Arrange
+        
+        AnagramFinder anagramFinder = new AnagramFinder("cat");
+        List<AnagramFinder> finalResult = new List<AnagramFinder> {anagramFinder};
+
+        //Act
+        AnagramFinder.AddWordList(anagramFinder);
+        List<AnagramFinder> result = AnagramFinder.GetAll();
+
+        //Assert
+        CollectionAssert.AreEqual(finalResult, result);
+
+      }
   }
 }
--- Anagram/Models/Anagram.cs
+++ Anagram/Models/Anagram.cs
@@ -1,13 +1,25 @@
+using System.Collections.Generic;
+using System;
+
+
 namespace Anagram.Models
 {
   public class AnagramFinder
   {
     public string Word { get; set; }
+    private static List<AnagramFinder> _instances = new List<AnagramFinder> {};
     public AnagramFinder(string word)
     {
       Word = word;
+      // _instances.Add(this);
     }
-    
-
+    public static void AddWordList(AnagramFinder matchingObj)
+    {
+      _instances.Add(matchingObj);
+    }
+    public static List<AnagramFinder> GetAll()
+    {
+      return _instances;
+    }
   }
 }
