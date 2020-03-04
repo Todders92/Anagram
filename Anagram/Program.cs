cb99310e5a7ac89c6303f9540dd09a56a7248c7f
Brandan Sayarath and Todd Walraven <brandan@>
2020-03-03 16:41:18 -0800

    begin adding frontend logic

--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -8,7 +8,12 @@ namespace Program
   {
     public static void Main()
     {
-      
+      Console.WriteLine("enter a word to find the anagrams of it");
+      string word = Console.ReadLine();
+      Console.WriteLine("enter words you thinkn may be anagrams of your inputted word");
+      string compareWord = Console.ReadLine();
+      AnagramFinder anagramFinder = new AnagramFinder(word);
+      Console.WriteLine(anagramFinder.Word);
     }
   }
 }
